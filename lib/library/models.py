6f3b100a788fa974f7f179dd1cd5dba486b5df5d
Akshay <akshay@Akshays-MacBook-Air.local>
2025-12-28 14:13:14 +1300

    initial commit, added reader, book, loan models version 0.0.1

---
+++ lib/library/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class Reader(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    date_of_membership = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+    available_for_loan = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title} (copy: {self.id})"
+    
+class Loan(models.Model):
+    reader = models.ForeignKey(Reader, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    loan_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.book.title} loaned to {self.reader.name}"
